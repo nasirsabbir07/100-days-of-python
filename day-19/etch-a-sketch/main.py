9c2a70cd8a8bacec48a1d525735a5ad7a456ff61
Nasir <btbsabbirnas@barytech.com>
2024-08-29 01:38:01 +0600

    create and etch a sketch

--- day-19/etch-a-sketch/main.py
+++ day-19/etch-a-sketch/main.py
@@ -7,7 +7,23 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def rotate_left():
+    tim.left(10)
+
+def rotate_right():
+    tim.right(10)
+
+def erase_drawing():
+    tim.reset()
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=rotate_left)
+screen.onkey(key="d", fun=rotate_right)
+screen.onkey(key="c", fun=erase_drawing)
+
 screen.exitonclick()
