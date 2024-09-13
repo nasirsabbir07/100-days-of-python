78bffe4c242df0f05b947032364004e54401eb1f
Nasir <btbsabbirnas@barytech.com>
2024-09-14 04:57:19 +0600

    - Update the tail collision logic

--- day-20/snake_game/main.py
+++ day-20/snake_game/main.py
@@ -39,11 +39,8 @@ while game_is_on: # run as long as it is true
         score.game_over()
 
     #Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             score.game_over()
 
